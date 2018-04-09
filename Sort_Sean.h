19e1dc60d5a4bfcb470c0ead1ae64d19370d4a55
Sean Hearne <hearnesc@mail.uc.edu>
2018-04-09 14:43:05 -0400

    Starting sean

--- Sort_Sean.h
+++ Sort_Sean.h
@@ -1,1 +1,23 @@
 #pragma once
+int arr[10];
+arr[0] = 5;
+arr[1] = 2;
+arr[2] = 7;
+arr[3] = 15;
+arr[4] = 19;
+arr[5] = 1;
+arr[6] = 20;
+arr[7] = 8;
+arr[8] = 3;
+arr[9] = 17;
+void mergeSort(int data[]) {
+	int length = len(data);
+}
+
+int main() {
+
+	mergeSort(arr);
+	cout << "end";
+
+	return 0
+}
