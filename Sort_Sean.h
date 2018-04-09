8d40a0cd25eac9ce953a41fe1c928d7be578b327
Sean Hearne <hearnesc@mail.uc.edu>
2018-04-09 16:35:09 -0400

    Working on Merge sort

--- Sort_Sean.h
+++ Sort_Sean.h
@@ -1,23 +1,42 @@
 #pragma once
-int arr[10];
-arr[0] = 5;
-arr[1] = 2;
-arr[2] = 7;
-arr[3] = 15;
-arr[4] = 19;
-arr[5] = 1;
-arr[6] = 20;
-arr[7] = 8;
-arr[8] = 3;
-arr[9] = 17;
-void mergeSort(int data[]) {
-	int length = len(data);
+#include <iostream>
+
+using namespace std;
+void mergeSort(int data[], int start, int end, int n);
+void mergeSort(int data[], int n) {
+	mergeSort(data, 0, n / 2,n);
+	mergeSort(data, n / 2, n,n);
 }
-
-int main() {
-
-	mergeSort(arr);
-	cout << "end";
-
-	return 0
+void mergeSort(int data[], int start, int end, int n) {
+	if (end-start > 2) {
+		mergeSort(data, start, ((end - start) / 2) + start,n);
+		mergeSort(data, ((end-start) / 2) + start, end,n);
+	}
+	int *temp = new int[end-start];
+	int i = start;
+	int j = ((end - start) / 2);
+	int t = 0;
+	while ( t < end) {
+		if (i == ((end - start) / 2)) {
+			temp[t] = data[j];
+			j++;
+		}
+		else if (j == end) {
+			temp[t] = data[i];
+			i++;
+		}
+		else if (data[i] < data[j]) {
+			temp[t] = data[i];
+			i++;
+		}
+		else {
+			temp[t] = data[j];
+			j++;
+		}
+		t++;
+	}
+	for (int x = 0; x < end-start; x++) {
+		cout << x + start << " set to " << temp[x] << endl;
+		data[x+start] = temp[x];
+	}
 }
