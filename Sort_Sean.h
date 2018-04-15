a61e7ec5f3285e2842d73c67b1da8f0deb7f00ef
Sean Hearne <hearnesc@mail.uc.edu>
2018-04-15 19:19:47 -0400

    MergeSort finished

--- Sort_Sean.h
+++ Sort_Sean.h
@@ -2,26 +2,18 @@
 #include <iostream>
 
 using namespace std;
-void mergeSort(int data[], int start, int end, int n);
-void mergeSort(int data[], int n) {//n = length of array
-	mergeSort(data, 0, n / 2,n);//spliting array in two
-	mergeSort(data, n / 2, n,n);
-}
-void mergeSort(int data[], int start, int end, int n) {//start is the number the split begins and end is the end but not included
-	if (end-start > 2) {//if length of split is larger than 2 split again
-		mergeSort(data, start, ((end - start) / 2) + start,n);
-		mergeSort(data, ((end-start) / 2) + start, end,n);
-	}
-	int *temp = new int[end-start];//used to store sorted values 
-	int i = start;//used to keep track of postion in start half
-	int j = ((end - start) / 2)+start;//end-start gives length divding it in half gives how far it from start it begins 
-	int t = 0;//used to keep track of position in temp
-	while ( t < end) {
-		if (i == ((end - start) / 2)+start) {//if there are no more spots in the start half
+void Merge(int data[], int start,int  end) {
+	int mid = (end + start) / 2;
+	int *temp = new int[end-start+1]();//used to store sorted values 
+	int i = start;			//used to keep track of postion in start half
+	int j = mid + 1;			//used to keep track of postion in end half
+	int t = 0;				//used to keep track of position in temp
+	while (t < end - start + 1) {
+		if (i >= mid) {//if there are no more spots in the start half
 			temp[t] = data[j];
 			j++;
 		}
-		else if (j == end) {//if there are no more spots in the end half
+		else if (j >= end) {//if there are no more spots in the end half
 			temp[t] = data[i];
 			i++;
 		}
@@ -35,9 +27,24 @@ void mergeSort(int data[], int start, int end, int n) {//start is the number the
 		}
 		t++;
 	}
-	for (int x = 0; x < end-start; x++) {//seting data to equal sorted temp
-		//cout << x + start << " set to " << temp[x] << endl;
-		data[x+start] = temp[x];
+	for (int x = start; x <= end; x++) {
+		data[i] = temp[i-start];
+	}
+}
+
+void mergeSort(int data[], int start, int end);
+void mergeSort(int data[], int n) {//n = length of array
+	mergeSort(data, 0, n / 2);//spliting array in two
+	mergeSort(data, n / 2, n);
+}
+
+void mergeSort(int data[], int start, int end) {//start is the number the split begins and end is the end but not included
+	if (start < end) {
+		int mid = (end + start) / 2;
+		mergeSort(data, start, mid);
+		mergeSort(data, mid, start);
+		Merge(data, start, end);
+
 	}
 }
 
